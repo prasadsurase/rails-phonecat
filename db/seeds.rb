3fb2ceb4f62c95d7c622b72d36031add2126d245
Prasad Surase <surase_prasad@yahoo.co.in>
2014-08-03 22:32:05 +0530

    added db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+file = File.read('data/phones.json')
+data = JSON.parse(file)
+Phone.destroy_all
+data.each do |record|
+  puts '-------------------------------------------------------'
+  puts '-------------------------------------------------------'
+  puts record
+  phone = File.read("data/#{record['id']}.json")
+  phone = JSON.parse(phone)
+  puts 'phone----------------------'
+  puts phone
+
+  Phone.create!(
+    name: record['name'], 
+    snippet: record['snippet'],
+    features: phone['additionalFeatures'].split('; ').join(', '),
+    os: phone['android']['os'].split(' ').first,
+    os_version: phone['android']['os'].split(' ').last.to_f
+  )
+end
